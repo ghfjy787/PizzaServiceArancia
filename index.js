fdeec3c30a949051a5d983b7f724b617d352836a
ivan <sonoivan2000@test.com>
2018-02-23 17:19:12 +0100

    dsvsd

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: req.body.result.contexts,
-    displayText: req.body.result.contexts,
+    speech: req.body.result.contexts.name,
+    displayText: req.body.result.contexts.name,
     source: "webhook-echo-sample"
   });
 });
