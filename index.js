ee8175b5ded1d24920871b6105f84f5296a326cb
ivan <sonoivan2000@test.com>
2018-02-23 17:13:08 +0100

    fff

--- index.js
+++ index.js
@@ -14,15 +14,9 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  /*var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";*/
   return res.json({
-    speech: "aaa", //req.body.result.contexts[0].name,
-    displayText: "aaa", //req.body.result.contexts[0].name,
+    speech: "aaa",
+    displayText: "aaa",
     source: "webhook-echo-sample"
   });
 });
