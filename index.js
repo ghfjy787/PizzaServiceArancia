a07bc24c9b9155f65284ced4b68169b2d2348ad5
ivan <sonoivan2000@test.com>
2018-02-23 17:38:34 +0100

    dvsa

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ restService.post("/echo", function(req, res) {
 
   if(context == "ordine") {
     for(var i = 0; i < tipiPizze.length; i++) {
-      _speech += " " + tipiPizze[i];
+      _speech += " " + tipiPizze[i] + ",";
     }
+
+    _speech += ".";
 
     return res.json({
       speech: _speech,
